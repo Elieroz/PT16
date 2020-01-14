66e959be9e93cdd0c4d7df69a0aecbf73765b9a7
bernat <bernatespinas@gmail.com>
2020-01-15 00:02:11 +0100

    Utilitzo Picasso per a descarregar i assignar imatges

--- app/src/main/java/com/example/pt16/BlocAdapter.java
+++ app/src/main/java/com/example/pt16/BlocAdapter.java
@@ -1,8 +1,5 @@
 package com.example.pt16;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +9,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.ref.WeakReference;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 public class BlocAdapter extends RecyclerView.Adapter<BlocAdapter.BlocViewHolder> {
@@ -44,15 +38,11 @@ public class BlocAdapter extends RecyclerView.Adapter<BlocAdapter.BlocViewHolder
 
         holder.hourBegin.setText(bloc.getHourBegin());
         holder.temperature.setText(Double.toString(bloc.getTemperature()));
-//        if (bloc.isCold()) {
-//            holder.icon.setImageResource(R.drawable.cold);
-//        } else {
-//            holder.icon.setImageResource(R.drawable.hot);
-//        }
-        new BlocAdapter.ImageLoad(
-                "http://openweathermap.org/img/wn/" + bloc.getIcon() + "@2x.png",
-                holder.icon
-        ).execute();
+
+        Picasso.get()
+                .load("http://openweathermap.org/img/wn/" + bloc.getIcon() + "@2x.png")
+                .into(holder.icon)
+        ;
     }
 
     @Override
@@ -71,39 +61,6 @@ public class BlocAdapter extends RecyclerView.Adapter<BlocAdapter.BlocViewHolder
             this.hourBegin = itemView.findViewById(R.id.hourBegin);
             this.temperature = itemView.findViewById(R.id.temperature);
             this.icon = itemView.findViewById(R.id.icon);
-        }
-    }
-
-    /**
-     * Una classe que descarrega una imatge en segon pla i l'assigna a un ImageView.
-     */
-    private static class ImageLoad extends AsyncTask<Void, Void, Bitmap> {
-        private String url;
-        private WeakReference<ImageView> icon;
-
-        public ImageLoad(String url, ImageView icon) {
-            this.url = url;
-            this.icon = new WeakReference<>(icon);
-        }
-
-        @Override
-        protected Bitmap doInBackground(Void... voids) {
-            try {
-                HttpURLConnection connection = (HttpURLConnection) new URL(this.url).openConnection();
-                connection.connect();
-
-                InputStream input = connection.getInputStream();
-                return BitmapFactory.decodeStream(input);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            this.icon.get().setImageBitmap(bitmap);
         }
     }
 }
