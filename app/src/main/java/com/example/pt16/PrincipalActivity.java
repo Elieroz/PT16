42af421dace5b9745f80e632b21092e9f1874236
bernat <bernatespinas@gmail.com>
2020-01-18 16:21:57 +0100

    Eliminat Floating Action Button

--- app/src/main/java/com/example/pt16/PrincipalActivity.java
+++ app/src/main/java/com/example/pt16/PrincipalActivity.java
@@ -52,14 +52,14 @@ public class PrincipalActivity extends AppCompatActivity implements View.OnClick
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         this.edtCity = findViewById(R.id.edtCity);
 
